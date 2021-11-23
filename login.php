65293a334b7a770652f8db7b25450335b4fca2d8
jt <jt@JT-Station.jtcima.com>
2021-11-23 20:44:29 +1100

    23112021

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+    <head>
+
+        <title>Chatroom</title>
+
+    </head>
+
+    <body>
+
+        <div style="height:100px; background-color: rgb(59,89,152); color: #d9dfeb;">
+            Chatroom Signup
+        </div>
+
+    </body>
+
+</html>
